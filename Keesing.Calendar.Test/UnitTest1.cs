0d0afde0fe05d0b7793392b3f2ab0414ac8c584a
Liron <Liron.levy@heroes.nl>
2020-03-21 22:30:08 +0100

    Create Web Api & Test Projects.
    Basic Web Api and Test project were created under one solution.

---
+++ Keesing.Calendar.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Keesing.Calendar.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
