d0f821af8b03048a5c34fc328fa10916806beeee
Liron <Liron.levy@heroes.nl>
2020-03-21 23:01:13 +0100

    Update Event.cs
    Creating event model.

---
+++ Keesing.Calendar.Api/Models/Event.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace keesing.Calendar.Api.Models
+{
+    public class Event
+    {
+        Event()
+        {
+            lock (_lockObj)
+            {
+                this.Id = _id++;
+            }
+        }
+
+        public string Name { get; set; }
+        public int Time { get; set; }
+        public string Location { get; set; }
+        public string Members { get; set; }
+        public string EventOrganizer { get; set; }
+        public int Id { get; set; }
+
+        private static object _lockObj = new object();
+        private static int _id = 0;
+    }
+}
