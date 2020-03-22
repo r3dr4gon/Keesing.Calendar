e32cbb8093ecc47e4a94880c74e174688e74ffd9
Liron <Liron.levy@heroes.nl>
2020-03-22 01:46:34 +0100

    Create CalendarServiceMock.cs
    Creating a simple Mock DB.

---
+++ Keesing.Calendar.Test/CalendarServiceMock.cs
@@ -0,0 +1,70 @@
+﻿using Keesing.Calendar.Api.Models;
+using Keesing.Calendar.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Keesing.Calendar.Test
+{
+    class CalendarServiceMockEmpty : ICalendarService
+    {
+        private IDictionary<int, Event> _events = new Dictionary<int, Event>();
+        private IDictionary<string, int> _secondaryKey = new Dictionary<string, int>();
+
+        public CalendarServiceMockEmpty() { }
+
+        public CalendarServiceMockEmpty(Event[] events)
+        {
+            
+        }
+
+        public void AddEvent(Event @event)
+        {
+            _secondaryKey.Add(@event.Name, @event.Id);
+            _events.Add(@event.Id, @event);
+        }
+
+        public bool EditEvent(Event @event)
+        {
+            if (!_events.ContainsKey(@event.Id))
+                return false;
+
+            PropertyInfo[] properties = typeof(Event).GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.GetValue(@event) != null)
+                    property.SetValue(_events[@event.Id], @event);
+            }
+
+            return true;
+        }
+
+        public bool DeleteEvent(int id)
+        {
+            if (!_events.ContainsKey(id))
+                return false;
+
+            _secondaryKey.Remove(_events[id].Name);
+            _events.Remove(id);
+            return true;
+        }
+
+        public Event GetEventById(int id)
+        {
+            return _events[id];
+        }
+
+        public Event GetEventByName(string name)
+        {
+            return _events[_secondaryKey[name]];
+        }
+
+        public Event[] GetAllEvents()
+        {
+            return _events.Values.ToArray();
+        }
+    }
+}
